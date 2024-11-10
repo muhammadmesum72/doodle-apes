89eb2f5f2768d106b88fdd216c27c38da1a95287
m_mesum <me.m.mesum@gmail.com>
2024-11-10 22:14:13 +0500

    hositing setup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,26 +4,26 @@ const Footer = () => {
       <div className="text-[24px] z-50">© D00dle Apes 2024</div>
       <div className="flex items-center gap-4 z-50">
         <div>
+          {/* x link */}
           <a href="https://x.com/D00dleApes" className="z-50" target="_blank">
-
-          <svg
-            width="31"
-            height="33"
-            viewBox="0 0 31 33"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M18.2115 14.088L29.6033 0.75H26.9032L17.0145 12.3307L9.1125 0.75H0L11.9475 18.264L0 32.25H2.7L13.1445 20.019L21.4898 32.25H30.6022L18.2115 14.088ZM14.5148 18.417L13.3043 16.6732L3.672 2.7975H7.81875L15.5903 13.9958L16.8008 15.7395L26.9055 30.297H22.7587L14.5148 18.417Z"
-              fill="white"
-            />
-          </svg>
+            <svg
+              width="31"
+              height="33"
+              viewBox="0 0 31 33"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M18.2115 14.088L29.6033 0.75H26.9032L17.0145 12.3307L9.1125 0.75H0L11.9475 18.264L0 32.25H2.7L13.1445 20.019L21.4898 32.25H30.6022L18.2115 14.088ZM14.5148 18.417L13.3043 16.6732L3.672 2.7975H7.81875L15.5903 13.9958L16.8008 15.7395L26.9055 30.297H22.7587L14.5148 18.417Z"
+                fill="white"
+              />
+            </svg>
           </a>
-
         </div>
         <div>
+          {/* magic eden Image */}
           <a href="https://magiceden.io/" target="_blank">
-          <img src="/images/magicEden.png" className="w-[36px]" alt="" />
+            <img src="/images/magicEden.png" className="w-[36px]" alt="" />
           </a>
         </div>
       </div>
